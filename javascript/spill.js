b357eeff0cd7878e1aebc8b47f33c09932e269e8
viktigsau <159922627+viktigsau@users.noreply.github.com>
2024-06-07 21:28:23 +0200

    Delete javascript directory

--- javascript/spill.js
+++
@@ -1,6 +0,0 @@
-const fangForet = {
-    setWidthAndHeight: function() {
-        document.getElementById("fangforetframe").style.width = document.getElementById("bredde").value + "px";
-        document.getElementById("fangforetframe").style.height = document.getElementById("høgde").value + "px";
-    },
-};
